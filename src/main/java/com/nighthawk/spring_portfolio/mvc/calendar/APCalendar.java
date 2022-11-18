9c256ae9c6080ede6a036999146a03e5d9854d62
samaya <samaya2024@gmail.com>
2022-11-17 23:55:19 -0800

    methods + endpoints

--- src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/APCalendar.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calendar/APCalendar.java
@@ -1,5 +1,6 @@
 package com.nighthawk.spring_portfolio.mvc.calendar;
-
+import java.util.Calendar;
+import java.util.*;
 // Prototype Implementation
 
 public class APCalendar {
@@ -29,10 +30,9 @@ public class APCalendar {
      * 6 denotes Saturday. 
      * firstDayOfYear(2019) returns 2 for Tuesday.
     */
-    private static int firstDayOfYear(int year) {
-        // implementation not shown
-
-        return 0;
+    static int firstDayOfYear(int year) {
+            Date currentDate = new Date(year-1900, 0, 1);
+            return currentDate.getDay();
         }
 
 
@@ -42,28 +42,35 @@ public class APCalendar {
      * dayOfYear(3, 1, 2017) returns 60, since 2017 is not a leap year
      * dayOfYear(3, 1, 2016) returns 61, since 2016 is a leap year. 
     */ 
-    private static int dayOfYear(int month, int day, int year) {
-        // implementation not shown
-
-        return 1;
+    public static int dayOfYear(int month, int day, int year) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(year, month-1, day);
+        int date = cal.get(Calendar.DAY_OF_YEAR);
+        return date;
         }
 
     /** Returns the number of leap years between year1 and year2, inclusive.
      * Precondition: 0 <= year1 <= year2
     */ 
     public static int numberOfLeapYears(int year1, int year2) {
-         // to be implemented in part (a)
-
-        return 0;
+        int num = 0;
+        for (int i = year1; i <= year2; i++){
+            if (isLeapYear(i)){
+                num += 1;
+            }
         }
+        return(num);
+    }
 
     /** Returns the value representing the day of the week for the given date
      * Precondition: The date represented by month, day, year is a valid date.
     */
     public static int dayOfWeek(int month, int day, int year) { 
-        // to be implemented in part (b)
-        return 0;
-        }
+        int firstDay = firstDayOfYear(year);
+        int date = dayOfYear(month, day, year) - 1;
+        int out = firstDay + date;
+        return(out % 7);
+    }
 
     /** Tester method */
     public static void main(String[] args) {
